f1de2398496a57572a6f310b7019046a307169a3
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-05-27 21:03:20 -0700

    Add sort in AddRankingVC

--- ran01/Controller/AddRankingVC.swift
+++ ran01/Controller/AddRankingVC.swift
@@ -12,35 +12,50 @@ class AddRankingVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var rankingNameLbl: UILabel!
+    @IBOutlet weak var addNextItemBtn: UIButton!
     
     var rankingKey: String = ""
     var rankItemsArray: [RankItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        
+        _setTableView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
+        _getRankingTitle()
+        _getAllRankItems()
+    }
+    
+    func _setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func _getRankingTitle() {
         DataService.instance.getRankingTitle(forRankingKey: rankingKey) { (returnedRankingTitle) in
             self.rankingNameLbl.text = returnedRankingTitle
+            self.tableView.reloadData()
         }
+    }
 
+    func _getAllRankItems() {
         DataService.instance.getAllRankItemsFor(rankingKey: rankingKey) { (returnedRankItem) in
             self.rankItemsArray = returnedRankItem
+            self.tableView.reloadData()
+            
+            DataService.instance.getNumberOfItems(forRankingKey: self.rankingKey, handler: { (numberOfItems) in
+                if self.rankItemsArray.count == numberOfItems {
+                    self.addNextItemBtn.isHidden = true
+                }
+            })
         }
     }
     
-    
-    
-    
     @IBAction func addNextItemBtnWasPressed(_ sender: Any) {
         performSegue(withIdentifier: "AddRankDetailVC", sender: nil)
-//        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func cancelBtnWasPressed(_ sender: Any) {
@@ -60,7 +75,6 @@ extension AddRankingVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return rankItemsArray.count + 1
         return rankItemsArray.count
     }
     
@@ -70,6 +84,8 @@ extension AddRankingVC: UITableViewDelegate, UITableViewDataSource {
             print("Fail to get addRankCell")
             return UITableViewCell()
         }
+        
+        rankItemsArray.sort(by: {$0.rank < $1.rank})
         
         if indexPath.row != rankItemsArray.count {
             let rankItem = rankItemsArray[indexPath.row]
@@ -87,13 +103,5 @@ extension AddRankingVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
      
         dismiss(animated: true, completion: nil)
-//        performSegue(withIdentifier: "AddRankDetailVC", sender: rankingKey)
-
     }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if let addRankDetailVC = segue.destination as? AddRankDetailVC {
-//            addRankDetailVC.rankingKey = sender as! String
-//        }
-//    }
 }
